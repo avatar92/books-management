474ab32b2848d96f502ec4253594893aa196bff1
avatar92 <adelbensalem1992@gmail.com>
2019-08-24 15:44:33 +0200

    Updates

---
+++ precache-manifest.8f21f23b1dc4fc3ec57317cf7a80654a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806d871dce09d2d71db13e3fc18c9037",
+    "url": "/books-management/index.html"
+  },
+  {
+    "revision": "abc126334e531a2d5123",
+    "url": "/books-management/static/css/main.057a26ef.chunk.css"
+  },
+  {
+    "revision": "21c3c7d79702ce1fbe0f",
+    "url": "/books-management/static/js/2.ce00776e.chunk.js"
+  },
+  {
+    "revision": "abc126334e531a2d5123",
+    "url": "/books-management/static/js/main.cc780fa5.chunk.js"
+  },
+  {
+    "revision": "572e7c8628fcfb31ba7f",
+    "url": "/books-management/static/js/runtime~main.371ff20d.js"
+  }
+]);
